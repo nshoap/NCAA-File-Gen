ba13bf47111b008c77021c6460424246e02d072c
nshoap <noahshoap@gmail.com>
2020-09-01 18:11:18 -0400

    Opening file and loading file into vector

--- Generator.cpp
+++ Generator.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <memory>
 
 int main(int argc, char** argv)
 {
-	std::cout << "NCAA Roster Generator\n";
+	std::ifstream file("CSV/PlayersExport.csv");	
+
+	if (!file)
+	{
+		std::cerr << "Something went wrong.\n";
+		return 1;
+	}
+
+	std::vector<std::vector<std::string>> players;
+
+	std::string line;
+	unsigned int line_number = 0;
+
+	while (std::getline(file, line))
+	{
+		players.push_back({});
+		if (line_number != 0)
+		{
+			std::stringstream parse(line);
+			std::string value;
+		
+			while (std::getline(parse, value, ','))
+			{
+				players.back().push_back(value);
+			}
+		}
+		++line_number;
+	}
 
 	return 0;
 }
